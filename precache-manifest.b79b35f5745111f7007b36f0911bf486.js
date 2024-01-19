f67371a95cd9b3c7bfc5f60c93dafd1dfaa8c2a8
Dinesh Gangathran <dinesh.gangatharan@gematik.de>
2024-01-19 09:50:30 +0100

    Updates

---
+++ precache-manifest.b79b35f5745111f7007b36f0911bf486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235962beb94328cca367f35b317b2cf0",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "376e94753b91f9e63b96",
+    "url": "/calendar/static/css/main.0d9bf8f5.chunk.css"
+  },
+  {
+    "revision": "2090a0ed97476c4fe751",
+    "url": "/calendar/static/js/2.8d8c0b09.chunk.js"
+  },
+  {
+    "revision": "3ab2e28c91a341c5c529",
+    "url": "/calendar/static/js/3.d72a15bf.chunk.js"
+  },
+  {
+    "revision": "376e94753b91f9e63b96",
+    "url": "/calendar/static/js/main.b3029683.chunk.js"
+  },
+  {
+    "revision": "a201e5d7b4225117b325",
+    "url": "/calendar/static/js/runtime~main.64d987ff.js"
+  }
+]);
